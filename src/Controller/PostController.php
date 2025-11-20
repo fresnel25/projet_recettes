b3c8fc929972a9d16d57a67861afcf448dc41c34
fresnel25 <ngaleufresnel@gmail.com>
2025-11-20 14:54:00 +0100

    manipulation des caches

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,29 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'app_post')]
-    public function index(HttpClientInterface $client): Response
+    public function index(HttpClientInterface $client, CacheInterface $cache): Response
     {
-        // 1. Faire une requête GET vers l'API
-        $response = $client->request(
-            'GET',
-            'https://api.escuelajs.co/api/v1/products'
-        );
+        // ----------------------------
+        // 1. Cache du traitement long
+        // ----------------------------
+        $texte = $cache->get('mon_texte_cache', function (ItemInterface $item) {
+            $item->expiresAfter(20); // expire dans 20 secondes
+            return $this->simuler_traitement_long();
+        });
 
-        // 2. Récupérer le contenu JSON sous forme de tableau PHP
-        $produits = $response->toArray();
+        // ----------------------------
+        // 2. Cache des posts API
+        // ----------------------------
+        $produits = $cache->get('posts_api_cache', function (ItemInterface $item) use ($client) {
+            $item->expiresAfter(20); // expire dans 20 secondes
 
-        // 3. Afficher le résultat en dd
-        /* dd($produits); */
+            // Appel API (lent la première fois)
+            $response = $client->request(
+                'GET',
+                'https://api.escuelajs.co/api/v1/products'
+            );
+
+            return $response->toArray();
+        });
 
         return $this->render('post/index.html.twig', [
             'produits' => $produits,
+            'texte'    => $texte
         ]);
     }
+
 
     #[Route('/post_with_token', name: 'app_post_with_token')]
     public function indexWithToken(HttpClientInterface $client): Response
@@ -66,5 +81,12 @@ class PostController extends AbstractController
         dd($posts);
 
         return new Response("OK");
+    }
+
+    public function simuler_traitement_long()
+    {
+        sleep(4);
+        $texte = "c’était long !!";
+        return $texte;
     }
 }
